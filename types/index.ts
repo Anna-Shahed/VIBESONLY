4ef1e57531faa870e795d52fed8b27f1a2dee95b
Anna Shahed <annashahed09@gmail.com>
2026-09-21 15:39:34 -0400

    Remove LocalPlaylist interface and related code

--- types/index.ts
+++ types/index.ts
@@ -1,18 +1,1 @@
 
-  track: Track;
-  score: number;                 // 0..1
-  reasons: string[];
-}
-
-export interface LocalPlaylist {
-  id: string;
-  name: string;
-  trackIds: string[];
-  createdAt: number;
-}
-
-
-}
-
-  addTracksToPlaylist(playlistId: string, trackIds: string[]): Promise<void>;
-}
